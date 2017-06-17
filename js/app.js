9aac8412a749d4f9b79702172061e6011c94346e
Kyle Coleman <kylecoleman@kyles-mbp.attlocal.net>
2017-06-17 17:20:33 -0500

    Dumped template

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".element").typed({
+        strings: ["Hello!", "Salut!", "Ciao!", "Olá!", "Bonjour!", "Hello!"],
+        typeSpeed: 20,
+        backSpeed: 10,
+        loop: false,
+        showCursor: true
+    });
+});
