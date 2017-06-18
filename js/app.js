a7641c3652303dd1ea09105a6cfb8cdf1f748301
Kyle Coleman <kylecoleman@Kyles-MacBook-Pro.local>
2017-06-18 11:05:42 -0500

    attempting to load particles.js

--- js/app.js
+++ js/app.js
@@ -7,3 +7,8 @@ $(function(){
         showCursor: true
     });
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
